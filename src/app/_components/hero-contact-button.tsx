08ba818b9807e7208b5a2747473d70365257b446
melwin <114172287+itzTedx@users.noreply.github.com>
2024-08-24 16:14:25 +0400

    Responsive Contact Form

--- src/app/_components/hero-contact-button.tsx
+++ src/app/_components/hero-contact-button.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { HoverBorderGradient } from '@/components/ui/hover-border-gradient'
+import { useState } from 'react'
 
+import { HoverBorderGradient } from '@/components/ui/hover-border-gradient'
 import ContactForm from '@/components/contact-form'
 import {
   Drawer,
@@ -11,10 +12,50 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
-import { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
 export default function ContactButton() {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
   const [open, setOpen] = useState<boolean>(false)
+
+  if (isDesktop)
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="bg-background"
+          >
+            <span className="font-bold flex items-center gap-1 hover:gap-3 text-sm transition-all text-secondary-foreground duration-200 ease-out px-4 md:px-6 py-2.5 rounded-[inherit] focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-secondary-foreground md:text-lg font-bricolage">
+              Contact me
+            </span>
+          </HoverBorderGradient>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-lg">
+          <DialogHeader>
+            <DialogTitle>Contact me</DialogTitle>
+            <DialogDescription>
+              {
+                "Feel free to contact me with any questions or concerns. I'm open to opportunities that match my skills and interests."
+              }
+            </DialogDescription>
+          </DialogHeader>
+          <ContactForm setOpen={setOpen} />
+        </DialogContent>
+      </Dialog>
+    )
+
   return (
     <Drawer noBodyStyles open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
