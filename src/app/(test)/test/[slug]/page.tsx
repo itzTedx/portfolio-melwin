4d1d9a8f388bddf4e96830e207398224ab6848a9
melwin <114172287+itzTedx@users.noreply.github.com>
2024-08-30 03:00:37 +0400

    Added: Default routes for project intercepting routes

---
+++ src/app/(test)/test/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function ProjectPage({ params }: { params: { slug: string } }) {
+  // const project = getProjectBySlug(params.slug);
+
+  // if (!project) notFound();
+
+  console.log(params);
+
+  return <div></div>;
+}
--- src/app/(test)/test/page.tsx
+++ src/app/(test)/test/page.tsx
@@ -1,30 +1,12 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
-import PostPopup from "./popup";
 
 const PostPage = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  const openPopup = () => setIsPopupOpen(true);
-  const closePopup = () => setIsPopupOpen(false);
-
   return (
     <div>
-      <button
-        onClick={openPopup}
-        className="rounded bg-blue-500 p-2 text-white"
-      >
-        View Post
-      </button>
-
-      {isPopupOpen && (
-        <PostPopup
-          imageUrl="https://example.com/your-image.jpg"
-          description="This is the image description."
-          onClose={closePopup}
-        />
-      )}
+      <Link href={`/projects/${"hello"}`}>Hello Test</Link>
     </div>
   );
 };
