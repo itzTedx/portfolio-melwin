f626c80c9fea27da8d24ad96efceb2e02d452bd0
melwin <114172287+itzTedx@users.noreply.github.com>
2024-08-30 01:56:06 +0400

    Added: RGB Button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/contact-form";
 export default function ContactPage() {
   return (
     <div className="flex min-h-[98dvh] items-center justify-center gap-y-6 px-6">
-      <div className="w-1/2">
+      <div className="max-w-screen-lg">
         <ContactForm />
       </div>
     </div>
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,16 +1,16 @@
-import Content from '@/components/layout/footer-content'
+import Content from "@/components/layout/footer-content";
 
 function Footer() {
   return (
     <div
       className="relative h-[80dvh] md:h-[350px] lg:h-[450px]"
-      style={{ clipPath: 'polygon(0% 0, 100% 0%, 100% 100%, 0 100%)' }}
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <div className="fixed bottom-0 h-[80dvh] md:h-[350px] lg:h-[450px] w-full">
+      <div className="fixed bottom-0 h-[80dvh] w-full bg-background md:h-[350px] lg:h-[450px]">
         <Content />
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
