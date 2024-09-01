72dd0a6f52aa2c0a3695d8ac3aade50ce3169253
melwin <114172287+itzTedx@users.noreply.github.com>
2024-09-01 19:53:44 +0400

    SEO: Base url fixed

--- src/utils/siteConfig.ts
+++ src/utils/siteConfig.ts
@@ -1,14 +1,14 @@
-import { SiteConfig } from '@/types'
+import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: 'Portfolio of Melwin Af - Graphic Designer',
-  shortName: 'Melwin Af',
+  name: "Portfolio of Melwin Af - Graphic Designer",
+  shortName: "Melwin Af",
   description:
-    'Energetic visual creator looking for the opportunity to achieve my goal. Looking for a challenging career in a reputable organization where I can utilize the best of my skills and experiences allowing career growth and contribution to growth of organization.',
+    "Energetic visual creator looking for the opportunity to achieve my goal. Looking for a challenging career in a reputable organization where I can utilize the best of my skills and experiences allowing career growth and contribution to growth of organization.",
 
-  url: 'https://melwinaf.vercel.app',
-  ogImage: '/og-image.jpg',
+  url: "https://melwin.vercel.app",
+  ogImage: "/og-image.jpg",
   links: {
-    twitter: 'https://x.com/itzTedx_',
+    twitter: "https://x.com/itzTedx_",
   },
-}
+};
