dc1156f9bd825a55b50136ad477ef12eac29740a
melwin <114172287+itzTedx@users.noreply.github.com>
2024-09-03 01:07:19 +0400

    metadata

--- src/app/@modal/(.)projects/[slug]/page.tsx
+++ src/app/@modal/(.)projects/[slug]/page.tsx
@@ -3,13 +3,55 @@ import { FullScreenModal } from "@/components/full-screen-modal";
 import { notFound } from "next/navigation";
 import ModalContent from "./_components/modal-content";
 import ModalImage from "./_components/modal-image";
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
   const projects = await getProjects();
+  const slugs = projects.map((project) => ({ slug: project.slug }));
 
-  return projects.map((project) => ({
-    slug: project.slug,
-  }));
+  return slugs;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const slug = params.slug;
+
+  const project = await getProjectBySlug(params.slug);
+
+  if (!project) return { title: "Projects of Melwin" };
+
+  const description = project.metadata.summary!.slice(0, 140) + "...";
+
+  return {
+    title: project?.metadata.title + "- Melwin Af",
+    description,
+    openGraph: {
+      type: "article",
+      images: {
+        url: project.metadata.image!,
+        alt: description,
+      },
+      url: `/projects/${slug}`,
+      title: project?.metadata.title,
+      description,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: project?.metadata.title + "- Melwin Af",
+      description,
+      images: {
+        url: project.metadata.image!,
+        alt: description,
+      },
+
+      site: "@itzTedx_",
+      creator: "@itzTedx_",
+    },
+    alternates: { canonical: `/projects/${slug}` },
+  };
 }
 
 export default async function ProjectPage({
